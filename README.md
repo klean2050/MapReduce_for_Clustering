76264523f6575e79c5b9eb9338bf9c31712f1622
Kleanthis Avramidis <k.avramidis@windowslive.com>
2021-10-06 00:14:22 -0700

    document code

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # MapReduce for k-means Clustering
 
-This repository contains a programming assignment in Python for the ECE NTUA Course on Advanced Database Systems. A dataset with 13.000.000 recordings of taxi routes in New York is used in order to perform k-means clustering. The distributed program is run with Spark on the distributed Filesystem HDFS. Results are written in .csv format.
+This repository contains a programming assignment in Python for the ECE NTUA Course on Advanced Database Systems. A dataset with 13M recordings of taxi routes in New York is used in order to perform k-means clustering. The distributed program is run with Spark on the distributed Filesystem HDFS. Results are written in .csv format.
 
 
--- kmeans.py
+++ kmeans.py
@@ -6,9 +6,9 @@ from pyspark.sql import SparkSession
 sc = SparkSession.builder.appName("AdvDat").getOrCreate()
 
 def haversine(p,q):
-
-    """Calculate haversine distance based on coordinates p,q"""
-
+    """
+    Calculate haversine distance based on coordinates p,q
+    """
     lng1, lat1 = p
     lng2, lat2 = q
     dlng = abs(lng2-lng1)
@@ -20,53 +20,53 @@ def haversine(p,q):
 
 
 def coordinates(line):
-
-    """Produces coordinates tuples
-    line: CSV string from RDD"""
-
+    """
+    Produces coordinates tuples
+    line: CSV string from RDD
+    """
     contents = line.split(",")
     lng, lat = map(float,contents[3:5])
     return lng, lat
 
 
 def closest(centroids,coordinates):
-
-    """Computes the minimum haversine distance and saves centroid
+    """
+    Computes the minimum haversine distance and saves centroid
     centroids: contains the centroids as tuples (id,coordinates)
     coordinates: tuple of floats
         
     Returns id_coords: tuple (int,tuple)
     Tuple containing the id of the closest centroid
-    and the tuple of coordinates of this point"""
-    
+    and the tuple of coordinates of this point
+    """
     tup = [(cen[0], haversine(coordinates,cen[1])) for cen in centroids]
     distance = min(tup, key = lambda x:x[1])
     return (distance[0],coordinates)
 
 
 def sum_by_elem(p,q):
-
-    """Reduce Function, sums each coordinate of 2 items
+    """
+    Reduce Function, sums each coordinate of 2 items
     p,q: tuples of (tuple of floats: coordinates,int)
-    Returns tuple of (tuple of summed floats, summed int)"""
-
+    Returns tuple of (tuple of summed floats, summed int)
+    """
     p, num1 = p
     q, num2 = q
     tup = map(sum,zip(p,q))
     return (tuple(tup),num1+num2)
 
 def avg_by_elem(p):
-
-    """Averages the coordinates assigned to centroid
+    """
+    Averages the coordinates assigned to centroid
     p: tuple returned by sum_by_elem
-    Returns tuple of floats: mean of coordinates"""
-
+    Returns tuple of floats: mean of coordinates
+    """
     p, num = p
     avg = map(lambda x:x/num, p)
     return tuple(avg)
 
 
-data = sc.textFile("yellow_tripdata_1m.csv
+data = sc.textFile('yellow_tripdata_1m.csv')
 # get all coordinates as tuples and filter outliers
 population = data.map(coordinates).filter(lambda line: line[0] and line[1]).cache()
 
@@ -89,5 +89,3 @@ centroids = new_centroids.toDF(["id","centroid"])
 centroids.withColumn("centroid", centroids.centroid.cast("string"))\
          .coalesce(1).write\
          .csv("/Centroids",header="true",mode="overwrite",quote="")
-
-
